5b06e18852c866c782097e9433758a45f00adfc1
Raquel Rodriguez <rakela290@gamil.com>
2025-07-18 17:03:09 +0200

    Subida inicial del juego de memoria

---
+++ java/script.js
@@ -0,0 +1,72 @@
+const images = [
+'image-1.jpg',
+'image-2.jpg',
+'image-3.jpg',
+'image-4.jpg',
+'image-5.jpg',
+'image-6.jpg',
+]
+//crea las fotos dobles
+const cards =images.concat(images);
+//const cards= (...images...images)
+//crear funcion para barajar 
+
+function shuffle (){
+
+    for(let i = cards.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * (i + 1));
+        [cards[i], cards[j]] = [cards[j],cards[i]];
+    }
+}
+shuffle();
+
+const gameBoard = document.getElementById('game-board');
+
+cards.forEach((image,index)=>{
+    const card = document.createElement('div');
+    card.className ='memoryCard';
+    card.dataset.image = image;
+    card.dataset.indice = index;
+    gameBoard.appendChild(card);
+    card.addEventListener('click', flipcard);
+});
+let flippecard = []; // cartas volteadas
+let matchCards = []; // cartas emparejadas
+
+
+function flipcard(e) {
+    const card =  e.target;
+    if(flippecard.length<2 && !matchCards.includes(card)){
+    card.style.backgroundImage = "url('./asset/image/" + card.dataset.image + "')";
+    flippecard.push(card);
+    if(flippecard.length==2){
+        console.log(flippecard.length)
+        setTimeout(checkMatch, 500);
+     }
+    
+}   
+
+   
+}
+
+function checkMatch(){
+    const [card1, card2] = flippecard;
+
+    if(card1.dataset.image === card2.dataset.image && card1.dataset.indice != card2.dataset.indice){
+        matchCards.push(card1,card2);
+    
+
+        if(matchCards.length === cards.length){
+            alert("¡¡ felicidades has ganado");
+            
+          }
+        
+        }
+    else{
+        card1.style.backgroundImage = "";
+        card2.style.backgroundImage = "";
+    }
+
+    flippecard = [];
+    }
+
